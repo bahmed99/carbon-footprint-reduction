f60649020e6b259af91219b5cabbedc39a4eb882
REKHIS Rafik <rrekhis@enseirb-matmeca.fr>
2023-10-28 22:24:20 +0200

    add Color feature

--- src/server/src/main/java/projet/gl/server/color/ColorController.java
+++ src/server/src/main/java/projet/gl/server/color/ColorController.java
@@ -1,5 +1,54 @@
 package projet.gl.server.color;
 
-public class ColorController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/colors")
+public class ColorController {
+    private final ColorService colorService;
+
+    @Autowired
+    public ColorController(ColorService colorService) {
+        this.colorService = colorService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Color>> getAllColors() {
+        List<Color> colors = colorService.getAllColors();
+        return new ResponseEntity<>(colors, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Color> getColorById(@PathVariable Long id) {
+        return colorService.getColorById(id)
+                .map(color -> new ResponseEntity<>(color, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Color> createColor(@RequestBody Color color) {
+        Color createdColor = colorService.createColor(color);
+        return new ResponseEntity<>(createdColor, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Color> updateColor(@PathVariable Long id, @RequestBody Color updatedColor) {
+        Color updated = colorService.updateColor(id, updatedColor);
+        if (updated != null) {
+            return new ResponseEntity<>(updated, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteColor(@PathVariable Long id) {
+        colorService.deleteColor(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
