d45a97bf450ff9265dfce8fa222a4ca6de3109e1
BEN SAAD Mahmoud <mbensaad@bordeaux-inp.fr>
2023-10-30 23:51:00 +0100

    CRUD for all entities

--- src/server/src/main/java/projet/gl/server/configuration/ConfigurationService.java
+++ src/server/src/main/java/projet/gl/server/configuration/ConfigurationService.java
@@ -1,5 +1,49 @@
 package projet.gl.server.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ConfigurationService {
-    
+
+    private final ConfigurationRepository configurationRepository;
+
+    @Autowired
+    public ConfigurationService(ConfigurationRepository configurationRepository) {
+        this.configurationRepository = configurationRepository;
+    }
+
+    public List<Configuration> getAllConfigurations() {
+        return configurationRepository.findAll();
+    }
+
+    public Configuration getConfigurationById(Long id) {
+        Optional<Configuration> configuration = configurationRepository.findById(id);
+        return configuration.orElse(null);
+    }
+
+    public Configuration createConfiguration(Configuration configuration) {
+        return configurationRepository.save(configuration);
+    }
+
+    public Configuration updateConfiguration(Long id, Configuration newConfiguration) {
+        Optional<Configuration> existingConfiguration = configurationRepository.findById(id);
+        if (existingConfiguration.isPresent()) {
+            Configuration configuration = existingConfiguration.get();
+            configuration.setName(newConfiguration.getName());
+            return configurationRepository.save(configuration);
+        }
+        return null;
+    }
+
+    public boolean deleteConfiguration(Long id) {
+        if (configurationRepository.existsById(id)) {
+            configurationRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
