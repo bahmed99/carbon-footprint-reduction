ce437db780cd31316c36a93db0f4809898f98feb
REKHIS Rafik <rrekhis@enseirb-matmeca.fr>
2023-10-30 20:12:07 +0100

    add documentation

--- src/server/src/main/java/projet/gl/server/auth/AuthenticationController.java
+++ src/server/src/main/java/projet/gl/server/auth/AuthenticationController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@Tag(name = "Authentication", description = "Authentication API")
 public class AuthenticationController {
 
     private final AuthenticationService service;
