c421f1aef6fb0ba956f48c94ad0f4caa7fdc4cae
mohamedbahloul <bahloulmohamedd@gmail.com>
2023-10-30 16:42:40 +0100

    add navbar , home page and page 404

--- src/client/src/MainRouter.jsx
+++ src/client/src/MainRouter.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Test from './pages/Test'
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Page404 from "./pages/Page404";
+import Test from "./pages/Test";
 
 export default function MainRouter() {
   return (
     <div>
-        <Routes>
-            <Route path={"/"} element={<Test/>} />
-        </Routes>
+      <Navbar />
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+
+
+        <Route path="/404" element={<Page404 />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
     </div>
-  )
+  );
 }
