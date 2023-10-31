1d81908821380e920c0b7aa45dc721f2f8eddba5
mohamedbahloul <bahloulmohamedd@gmail.com>
2023-10-31 19:54:42 +0100

    finish link authentification backend with frontend

--- src/client/src/pages/RegisterPage.jsx
+++ src/client/src/pages/RegisterPage.jsx
@@ -4,13 +4,22 @@ import RegistrationForm from "../components/RegistrationForm";
 import LogoImg from "../assets/images/logo2.png";
 
 const RegisterPage = () => {
-    return (
-        <div className="register-page-container">
-        <img  className="register-page-logo" src={LogoImg} alt="Logo" />
-        <RegistrationForm />
-        <h2 className="already-registred"> Vous êtes déjà inscrit ? <Link to="/login" className="link-to-login" > Connectez-vous ici </Link> </h2>
-        </div>
-    );
-}
+  return (
+    <div className="register-page-container">
+      <a className="register-page-logo-link" href="/">
+        <img className="register-page-logo" src={LogoImg} alt="Logo" />
+      </a>
+      <RegistrationForm />
+      <h2 className="already-registred">
+        {" "}
+        Vous êtes déjà inscrit ?{" "}
+        <Link to="/login" className="link-to-login">
+          {" "}
+          Connectez-vous ici{" "}
+        </Link>{" "}
+      </h2>
+    </div>
+  );
+};
 
 export default RegisterPage;
--- src/server/src/main/java/projet/gl/server/auth/AuthenticationController.java
+++ src/server/src/main/java/projet/gl/server/auth/AuthenticationController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
