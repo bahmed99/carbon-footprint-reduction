0c8925811cf54f9a5061c41b398b866e4a20f415
REKHIS Rafik <rrekhis@enseirb-matmeca.fr>
2023-10-31 18:43:10 +0100

    disable authorization

--- src/server/src/main/java/projet/gl/server/auth/SecurityConfiguration.java
+++ src/server/src/main/java/projet/gl/server/auth/SecurityConfiguration.java
@@ -27,19 +27,19 @@ public class SecurityConfiguration {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 // Authorize all requests
-                // .authorizeHttpRequests(req -> req.requestMatchers("/**")
+                .authorizeHttpRequests(req -> req.requestMatchers("/**")
                 // Authorize only /auth/** and documentation
-                .authorizeHttpRequests(req -> req.requestMatchers("/auth/**",
-                        "/v2/api-docs",
-                        "/v3/api-docs",
-                        "/v3/api-docs/**",
-                        "/swagger-resources",
-                        "/swagger-resources/**",
-                        "/configuration/ui",
-                        "/configuration/security",
-                        "/swagger-ui/**",
-                        "/webjars/**",
-                        "/swagger-ui.html")
+                // .authorizeHttpRequests(req -> req.requestMatchers("/auth/**",
+                //         "/v2/api-docs",
+                //         "/v3/api-docs",
+                //         "/v3/api-docs/**",
+                //         "/swagger-resources",
+                //         "/swagger-resources/**",
+                //         "/configuration/ui",
+                //         "/configuration/security",
+                //         "/swagger-ui/**",
+                //         "/webjars/**",
+                //         "/swagger-ui.html")
                         .permitAll()
                         .anyRequest()
                         .authenticated())
