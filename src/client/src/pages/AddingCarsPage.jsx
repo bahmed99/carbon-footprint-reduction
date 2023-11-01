eb9340b42b426405d9583c5192e2fa8d05d4dc73
BAHRI Ahmed <ahmed.bahri@bordeaux-inp.fr>
2023-11-01 21:55:43 +0100

    add navbar

--- src/client/src/pages/AddingCarsPage.jsx
+++ src/client/src/pages/AddingCarsPage.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import AddingCarsForm from '../components/AddingCarsForm';
 import Carpage from '../assets/images/carpage.png'
+import Navbar from '../components/Navbar';
 const AddingCarsPage = () => {
     return (
-        <div className='adding-cars-page-container'>
-            <img className='adding-cars-page-img' src={Carpage} alt='carpage' />
-            <h1 className='adding-cars-page-title'>Welcome to our vehicle registration form.</h1>
-<h3 className='adding-cars-page-subtitle'>Please fill out the fields below to register your car.</h3>
-
-            <AddingCarsForm />
-        </div>
+        <>
+            <Navbar />
+            <div className='adding-cars-page-container'>
+                <img className='adding-cars-page-img' src={Carpage} alt='carpage' />
+                <h1 className='adding-cars-page-title'>Welcome to our vehicle registration form.</h1>
+                <h3 className='adding-cars-page-subtitle'>Please fill out the fields below to register your car.</h3>
+                <AddingCarsForm />
+            </div>
+        </>
     );
 };
 
