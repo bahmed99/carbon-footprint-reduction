f818f4d60a9a8b6f5c18cb151ef76c9491cea418
BAHRI Ahmed <ahmed.bahri@bordeaux-inp.fr>
2023-11-05 02:15:03 +0100

    fix bug

--- src/server/src/main/java/projet/gl/server/vehicle/VehicleRepository.java
+++ src/server/src/main/java/projet/gl/server/vehicle/VehicleRepository.java
@@ -12,7 +12,7 @@ import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
-        @Query("SELECT v FROM Vehicle v " +
+        @Query("SELECT DISTINCT v FROM Vehicle v " +
                         "LEFT JOIN v.color c " +
                         "LEFT JOIN v.model m " +
                         "LEFT JOIN v.configurations conf " +
@@ -24,7 +24,7 @@ public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
                         "AND (:#{#vehiculeFilterDTO.brandIds} IS NULL OR m.brand.id IN (:#{#vehiculeFilterDTO.brandIds}))")
         List<Vehicle> findByFilters(VehiculeFilterDTO vehiculeFilterDTO);
 
-        @Query("SELECT v FROM Vehicle v " +
+        @Query("SELECT DISTINCT v FROM Vehicle v " +
                         "LEFT JOIN v.color c " +
                         "LEFT JOIN v.model m " +
                         "LEFT JOIN v.configurations conf " +
@@ -36,70 +36,67 @@ public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
                         "AND (:#{#vehiculeFilterDTO.brandIds} IS NULL OR m.brand.id IN (:#{#vehiculeFilterDTO.brandIds}))")
         Page<Vehicle> findByFiltersAndPageSize(VehiculeFilterDTO vehiculeFilterDTO, Pageable pageable);
 
-        @Query("SELECT v.model.id AS modelId, COUNT(v) AS vehicleCount FROM Vehicle v GROUP BY v.model.id")
+        @Query("SELECT v.model.id AS modelId, COUNT(DISTINCT v.id) AS vehicleCount FROM Vehicle v GROUP BY v.model.id")
         List<Object[]> countVehiclesByModel();
 
-        @Query("SELECT m.brand.id AS brandId, COUNT(v) AS vehicleCount FROM Vehicle v " +
+        @Query("SELECT m.brand.id AS brandId, COUNT(DISTINCT v.id) AS vehicleCount FROM Vehicle v " +
                         "LEFT JOIN v.model m " +
                         "GROUP BY m.brand.id")
         List<Object[]> countVehiclesByBrand();
 
+        // findByFiltersByBrand
 
-        //findByFiltersByBrand
-
-        @Query("SELECT m.brand.id AS brandId, COUNT(v) AS vehicleCount FROM Vehicle v " +
-                "LEFT JOIN v.color c " +
-                "LEFT JOIN v.model m " +
-                "LEFT JOIN v.configurations conf " +
-                "WHERE (:#{#vehicleFilterDTO.colorIds} IS NULL OR c.id IN (:#{#vehicleFilterDTO.colorIds})) " +
-                "AND (:#{#vehicleFilterDTO.modelIds} IS NULL OR m.id IN (:#{#vehicleFilterDTO.modelIds})) " +
-                "AND (:#{#vehicleFilterDTO.configurationIds} IS NULL OR conf.id IN (:#{#vehicleFilterDTO.configurationIds})) " +
-                "GROUP BY m.brand.id")
+        @Query("SELECT m.brand.id AS brandId, COUNT(DISTINCT v.id) AS vehicleCount FROM Vehicle v " +
+                        "LEFT JOIN v.color c " +
+                        "LEFT JOIN v.model m " +
+                        "LEFT JOIN v.configurations conf " +
+                        "WHERE (:#{#vehicleFilterDTO.colorIds} IS NULL OR c.id IN (:#{#vehicleFilterDTO.colorIds})) " +
+                        "AND (:#{#vehicleFilterDTO.modelIds} IS NULL OR m.id IN (:#{#vehicleFilterDTO.modelIds})) " +
+                        "AND (:#{#vehicleFilterDTO.configurationIds} IS NULL OR conf.id IN (:#{#vehicleFilterDTO.configurationIds})) "
+                        +
+                        "GROUP BY m.brand.id")
         List<Object[]> findByFiltersByBrand(VehiculeFilterDTO vehicleFilterDTO);
 
+        @Query("SELECT c.id AS ColorId, COUNT(DISTINCT v.id) AS vehicleCount FROM Vehicle v " +
+                        "LEFT JOIN v.color c " +
+                        "LEFT JOIN v.model m " +
+                        "LEFT JOIN v.configurations conf " +
+                        "WHERE (:#{#vehicleFilterDTO.colorIds} IS NULL OR c.id IN (:#{#vehicleFilterDTO.colorIds})) " +
+                        "AND (:#{#vehicleFilterDTO.modelIds} IS NULL OR m.id IN (:#{#vehicleFilterDTO.modelIds})) " +
+                        "AND (:#{#vehicleFilterDTO.configurationIds} IS NULL OR conf.id IN (:#{#vehicleFilterDTO.configurationIds})) "
+                        +
+                        "GROUP BY c.id")
+        List<Object[]> findByFiltersByColor(VehiculeFilterDTO vehicleFilterDTO);
 
-        @Query("SELECT c.id AS ColorId, COUNT(v) AS vehicleCount FROM Vehicle v " +
-        "LEFT JOIN v.color c " +
-        "LEFT JOIN v.model m " +
-        "LEFT JOIN v.configurations conf " +
-        "WHERE (:#{#vehicleFilterDTO.colorIds} IS NULL OR c.id IN (:#{#vehicleFilterDTO.colorIds})) " +
-        "AND (:#{#vehicleFilterDTO.modelIds} IS NULL OR m.id IN (:#{#vehicleFilterDTO.modelIds})) " +
-        "AND (:#{#vehicleFilterDTO.configurationIds} IS NULL OR conf.id IN (:#{#vehicleFilterDTO.configurationIds})) " +
-        "GROUP BY c.id")
-        List<Object[]>findByFiltersByColor(VehiculeFilterDTO vehicleFilterDTO);
+        @Query("SELECT conf.id AS ConfigurationId, COUNT(DISTINCT v.id) AS vehicleCount FROM Vehicle v " +
+                        "LEFT JOIN v.color c " +
+                        "LEFT JOIN v.model m " +
+                        "LEFT JOIN v.configurations conf " +
+                        "WHERE (:#{#vehicleFilterDTO.colorIds} IS NULL OR c.id IN (:#{#vehicleFilterDTO.colorIds})) " +
+                        "AND (:#{#vehicleFilterDTO.modelIds} IS NULL OR m.id IN (:#{#vehicleFilterDTO.modelIds})) " +
+                        "AND (:#{#vehicleFilterDTO.configurationIds} IS NULL OR conf.id IN (:#{#vehicleFilterDTO.configurationIds})) "
+                        +
+                        "GROUP BY conf.id")
+        List<Object[]> findByFiltersByConfigurations(VehiculeFilterDTO vehicleFilterDTO);
 
+        @Query("SELECT m.id AS ModelId, COUNT(DISTINCT v.id) AS vehicleCount FROM Vehicle v " +
+                        "LEFT JOIN v.color c " +
+                        "LEFT JOIN v.model m " +
+                        "LEFT JOIN v.configurations conf " +
+                        "WHERE (:#{#vehicleFilterDTO.colorIds} IS NULL OR c.id IN (:#{#vehicleFilterDTO.colorIds})) " +
+                        "AND (:#{#vehicleFilterDTO.modelIds} IS NULL OR m.id IN (:#{#vehicleFilterDTO.modelIds})) " +
+                        "AND (:#{#vehicleFilterDTO.configurationIds} IS NULL OR conf.id IN (:#{#vehicleFilterDTO.configurationIds})) "
+                        +
+                        "GROUP BY m.id")
+        List<Object[]> findByFiltersByModel(VehiculeFilterDTO vehicleFilterDTO);
 
-        @Query("SELECT conf.id AS ConfigurationId, COUNT(v) AS vehicleCount FROM Vehicle v " +
-        "LEFT JOIN v.color c " +
-        "LEFT JOIN v.model m " +
-        "LEFT JOIN v.configurations conf " +
-        "WHERE (:#{#vehicleFilterDTO.colorIds} IS NULL OR c.id IN (:#{#vehicleFilterDTO.colorIds})) " +
-        "AND (:#{#vehicleFilterDTO.modelIds} IS NULL OR m.id IN (:#{#vehicleFilterDTO.modelIds})) " +
-        "AND (:#{#vehicleFilterDTO.configurationIds} IS NULL OR conf.id IN (:#{#vehicleFilterDTO.configurationIds})) " +
-        "GROUP BY conf.id")
-        List<Object[]>findByFiltersByConfigurations(VehiculeFilterDTO vehicleFilterDTO);
-
-
-        @Query("SELECT m.id AS ModelId, COUNT(v) AS vehicleCount FROM Vehicle v " +
-        "LEFT JOIN v.color c " +
-        "LEFT JOIN v.model m " +
-        "LEFT JOIN v.configurations conf " +
-        "WHERE (:#{#vehicleFilterDTO.colorIds} IS NULL OR c.id IN (:#{#vehicleFilterDTO.colorIds})) " +
-        "AND (:#{#vehicleFilterDTO.modelIds} IS NULL OR m.id IN (:#{#vehicleFilterDTO.modelIds})) " +
-        "AND (:#{#vehicleFilterDTO.configurationIds} IS NULL OR conf.id IN (:#{#vehicleFilterDTO.configurationIds})) " +
-        "GROUP BY m.id")
-        List<Object[]>findByFiltersByModel(VehiculeFilterDTO vehicleFilterDTO);
-        
-
-        @Query("SELECT COUNT(v) FROM Vehicle v " +
-        "LEFT JOIN v.color c " +
-        "LEFT JOIN v.model m " +
-        "LEFT JOIN v.configurations conf " +
-        "WHERE (:#{#vehicleFilterDTO.colorIds} IS NULL OR c.id IN (:#{#vehicleFilterDTO.colorIds})) " +
-        "AND (:#{#vehicleFilterDTO.modelIds} IS NULL OR m.id IN (:#{#vehicleFilterDTO.modelIds})) " +
-        "AND (:#{#vehicleFilterDTO.configurationIds} IS NULL OR conf.id IN (:#{#vehicleFilterDTO.configurationIds})) ")
+        @Query("SELECT COUNT(DISTINCT v.id) FROM Vehicle v " +
+                        "LEFT JOIN v.color c " +
+                        "LEFT JOIN v.model m " +
+                        "LEFT JOIN v.configurations conf " +
+                        "WHERE (:#{#vehicleFilterDTO.colorIds} IS NULL OR c.id IN (:#{#vehicleFilterDTO.colorIds})) " +
+                        "AND (:#{#vehicleFilterDTO.modelIds} IS NULL OR m.id IN (:#{#vehicleFilterDTO.modelIds})) " +
+                        "AND (:#{#vehicleFilterDTO.configurationIds} IS NULL OR conf.id IN (:#{#vehicleFilterDTO.configurationIds})) ")
         long countByFilter(VehiculeFilterDTO vehicleFilterDTO);
-
-
 
 }
