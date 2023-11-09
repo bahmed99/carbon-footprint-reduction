03cc09e57962d38e366d03dfd0c2ca234aeb8f89
BEN SAAD Mahmoud <mbensaad@bordeaux-inp.fr>
2023-11-09 11:35:44 +0100

    checkToken draft

--- src/server/src/main/java/projet/gl/server/auth/AuthenticationController.java
+++ src/server/src/main/java/projet/gl/server/auth/AuthenticationController.java
@@ -1,14 +1,20 @@
 package projet.gl.server.auth;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.security.Principal;
+import projet.gl.server.user.User;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -29,6 +35,31 @@ public class AuthenticationController {
     public ResponseEntity<AuthenticationResponse> login(
             @RequestBody AuthenticationRequest request) {
         return ResponseEntity.ok(service.login(request));
+    }
+
+    
+    
+    @GetMapping("/getUsername")
+    public String getUsernameFromToken(@RequestParam("token") String token) {
+        String username = service.getUsernameFromToken(token);
+        if (username != null) {
+            return "Username: " + username;
+        } else {
+            return "Invalid token or expired token";
+        }
+    }
+
+    @GetMapping("/checkToken")
+    public ResponseEntity<String> checkTokenValidity(@RequestParam("token") String token, Principal principal) {
+        if (principal != null) {
+            User user = (User) principal;
+    
+
+            if (service.isTokenValidForUser(token, user)) {
+                return ResponseEntity.ok("OK");
+            }
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid or expired token");
     }
 
 }
